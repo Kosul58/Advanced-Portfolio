0a94c61f268359f33a9c46662254e24b4fed777e
Kosul58 <kosulgrg@gmail.com>
2024-12-02 21:49:33 +0545

    a

--- Kosul/src/assets/Frontend/Projects.jsx
+++ Kosul/src/assets/Frontend/Projects.jsx
@@ -10,34 +10,42 @@ function Projects() {
       <h2>Explore My Recent</h2>
       <h1>Projects</h1>
       <div className="projectsx ">
-        <div className="project1 blockl">
-          <div className="projectimg">
-            <img src={img1} className="prjimgx" alt="Ecomerce"></img>
-          </div>
-          <h1>Project1</h1>
-          <div className="projectinfo">
-            <button>Github</button>
-            <button>Live Demo</button>
-          </div>
-        </div>
-        <div className="project1 blocku">
-          <div className="projectimg">
-            <img src={img2} className="prjimgx" alt="Fitness"></img>
-          </div>
-          <h1>Project2</h1>
-          <div className="projectinfo">
-            <button>Github</button>
-            <button>Live Demo</button>
+        <div className="aboutl">
+          <div className="project1 blockl">
+            <div className="projectimg">
+              <img src={img1} className="prjimgx" alt="Ecomerce"></img>
+            </div>
+            <h1>Project1</h1>
+            <div className="projectinfo">
+              <button>Github</button>
+              <button>Live Demo</button>
+            </div>
           </div>
         </div>
-        <div className="project1 blockr">
-          <div className="projectimg">
-            <img src={img3} className="prjimgx" alt="Jakarta"></img>
+
+        <div className="aboutu">
+          <div className="project1 blocku">
+            <div className="projectimg">
+              <img src={img2} className="prjimgx" alt="Fitness"></img>
+            </div>
+            <h1>Project2</h1>
+            <div className="projectinfo">
+              <button>Github</button>
+              <button>Live Demo</button>
+            </div>
           </div>
-          <h1>Project3</h1>
-          <div className="projectinfo">
-            <button>Github</button>
-            <button>Live Demo</button>
+        </div>
+
+        <div className="aboutr">
+          <div className="project1 blockrr">
+            <div className="projectimg">
+              <img src={img3} className="prjimgx" alt="Jakarta"></img>
+            </div>
+            <h1>Project3</h1>
+            <div className="projectinfo">
+              <button>Github</button>
+              <button>Live Demo</button>
+            </div>
           </div>
         </div>
       </div>
