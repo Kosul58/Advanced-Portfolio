c167c01f6d4a5386fee7fa5128dd84c5edc23cbf
Kosul58 <kosulgrg@gmail.com>
2025-02-04 17:26:42 +0545

    aaaa

--- Kosul/vite.config.js
+++ Kosul/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Ensures Vite listens on all network interfaces
+    port: 3000, // You can specify a port if needed, 3000 is a common default
+  },
+});
